7287e001f55ae395fba65519289477c8431b813f
wang.haoyu <228233464@qq.com>
2021-04-23 10:11:30 +0800

    [build] 0.01.2

--- release.sh
+++ release.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 set -e
-echo "Enter release version: "
+echo "Enter major version: "
 read VERSION
-read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+read -p "Major $VERSION - are you sure? (y/n)" -n 1 -r
 echo  # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo "Releasing $VERSION ..."
+  echo "Major $VERSION ..."
 
   # commit
   git add -A
   git commit -m "[build] $VERSION"
-  npm version $VERSION --message "[release] $VERSION"
+  npm version $VERSION --message "[major] $VERSION"
   git push origin master
 
   # publish
